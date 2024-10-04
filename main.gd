b777c3795b80b8f752b7d46ae53c9daaec378bb8
grymmjack <grymmjack@gmail.com>
2024-10-04 13:01:05 -0400

    Got a bunch of ansis loading! woot.

--- main.gd
+++ main.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 	%ansi2.load_file("res://assets/ANSIs/US-CAT.ans")
 	%ansi2.screen.mode = %ansi2.screen.SCREEN_MODE.FONT_8x8
 	%ansi2.scale = Vector2(0.5, 0.5)
-	%ansi2.locate(%ansi1.size.x, 0)
+	%ansi2.locate(%ansi1.size.x + 800, 0)
 
 	%ansi3.load_file("res://assets/ANSIs/gj-cgagris.ans")
 	#%ansi3.scale = Vector2(0.5, 0.5)
@@ -22,7 +22,7 @@ func _ready() -> void:
 
 	%ansi4.load_file("res://assets/ANSIs/gj-dwimmer.ans")
 	#%ansi4.scale = Vector2(0.5, 0.5)
-	%ansi4.locate(0, %ansi3.position.y + %ansi3.size.y)
+	%ansi4.locate(350, %ansi3.position.y + %ansi3.size.y)
 
 	%ansi5.load_file("res://assets/ANSIs/gj-test1.ans")
 	%ansi5.screen.mode = %ansi5.screen.SCREEN_MODE.FONT_9x16
@@ -36,5 +36,10 @@ func _ready() -> void:
 	%ansi7.load_file("res://assets/ANSIs/bacsi-img2pal.png-25-OPT.ans")
 	#%ansi7.scale = Vector2(0.5, 0.5)
 	%ansi7.locate(1200, 0)
+
+	# TODO fix scale offset from center to top left corner
+	%ansi8.load_file("res://assets/ANSIs/gj-antonio.ans")
+	#%ansi8.scale = Vector2(0.5, 0.5)
+	%ansi8.locate(1600, %ansi6.position.y + %ansi6.size.y + 100)
 
 	#print_tree_pretty()
